5df5df6a5b95ac0d0872774e66bed5f3775136b1
sbkuehn <skuehn@10thmagnitude.com>
2017-03-16 20:29:41 +0000

    Added InSpec

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -4,15 +4,8 @@
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
+system_policies = 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System'
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
-end
-
-describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+describe registry_key('System Policies', system_policies) do 
+  its('EnableLUA') { should eq 0 }
 end
